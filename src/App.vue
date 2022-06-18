98adf5f58f5f6a29d5d5dd3d3087ce0883c5fbfd
Islam Osmonbekov <islam.o@veles.finance>
2022-06-19 01:37:10 +0600

    Added basic css styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h1>Popup Plugin</h1>
+
+  <button>
+    Show Popup
+  </button>
+
+
+  <Popup />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Popup  from '@/components/Popup';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="popup-wrapper">
+    <div class="popup-back"></div>
+    <div class="popup">
+      <!--THERE YOU CAN PASTE ANY HTML YOU WANT. START-->
+      <h1>Popup</h1>
+      <p>Click on black area out of popup to exit.</p>
+      <!--THERE YOU CAN PASTE ANY HTML YOU WANT. END-->
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.popup-wrapper {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+.popup-back {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 110%;
+  height: 110%;
+  backdrop-filter: blur(4px);
+  background: rgba(24, 10, 31, 0.7);
+  z-index: 1;
+}
+.popup {
+  width: fit-content;
+  background: white;
+  padding: 1rem;
+  box-shadow: 0 .4rem 2.6rem rgba(0, 0, 0, 0.12);
+  border-radius: 1.6rem;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+}
+
+@media (max-width: 720px) {
+  .popup {
+    width: calc(100% - 3.2rem);
+    height: max-content;
+    border-radius: 1.6rem;
+  }
+}
+</style>
+
